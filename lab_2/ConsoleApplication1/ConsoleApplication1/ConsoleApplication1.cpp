e19f8c9e1d5e2e8a9ac3311c683a58a429817af3
DubetsIvan <158566315+DubetsIvan@users.noreply.github.com>
2025-06-20 11:07:23 +0300

    lab 2 done

---
+++ lab_2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void car(int id) {
+    this_thread::sleep_for(chrono::milliseconds(1000));
+    int position = 0;
+    int direction = 0;
+    int speed = 100;
+    while (true) {
+        cout << "Car #" << id << " position: " << position << "\nCar #" << id << " speed: " << speed << endl;
+        if (position <= 0) {
+            direction = 0;
+        }
+        else if (position >= 400) {
+            direction = 1;
+        }
+        switch (direction)
+        {
+        case 0:
+            position += speed;
+            break;
+        case 1:
+            position -= speed;
+            break;
+        }
+        this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+}
+
+void motorcycle(int id) {
+    this_thread::sleep_for(chrono::milliseconds(500));
+    int direction = 0;
+    int position = 0;
+    int speed = 50;
+    while (true) {
+        cout << "Motorcycle #" << id << " position: " << position << "\nMotorcycle #" << id << " speed: " << speed << endl;
+        if (position <= 0) {
+            direction = 0;
+        }
+        else if (position >= 400) {
+            direction = 1;
+        }
+        switch (direction)
+        {
+        case 0:
+            position += speed;
+            break;
+        case 1:
+            position -= speed;
+            break;
+        }
+        this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+}
+
+int main()
+{
+    thread cars[3];
+    thread motorcycles[2];
+
+    for (int i = 0; i < 3; i++) {
+        cars[i] = thread(car, i + 1);
+    }
+
+    for (int i = 0; i < 2; i++) {
+        motorcycles[i] = thread(motorcycle, i + 1);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        cars[i].join();
+    }
+    for (int i = 0; i < 2; i++) {
+        motorcycles[i].join();
+    }
+
+    return 0;
+}
